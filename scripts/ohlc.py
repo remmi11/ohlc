4d20a1d2270cff65a89838c44055c1ae9438416d
remmi11 <wtgeographer@gmail.com>
2017-11-05 09:52:37 -0600

    updates with groupby functions

--- scripts/ohlc.py
+++ scripts/ohlc.py
@@ -1,162 +1,147 @@
 
 # coding: utf-8
-
-# In[1]:
-
+#-------------------------------------------------------------------------------
+# Name:        ohlc.py
+# Purpose:
+#
+# Author:      Noah Huntington
+#
+# Created:     11/05/2017
+# Copyright:   (c) Noah 2017
+# Licence:     MIT
+#-------------------------------------------------------------------------------
 
 import pandas as pd
-
-
-# In[2]:
-
-
 from datetime import datetime
-
-
-# In[3]:
-
 
 import sqlalchemy
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://postgres:4KS5CJlz0ZX8Po@localhost:5432/postgres')
+engine = create_engine('postgresql://postgres@localhost:5432/ohlc')
 
 
-# In[4]:
+def main():
+    
+    #10000 record test table
+    df = pd.read_sql_query("select * from test;", engine)
+
+    df = df.set_index(pd.DatetimeIndex(df['timestamp']))
+
+    df.head()
+
+    #5 MIN
+    five_min_summary = pd.DataFrame()
+
+    five_min_summary['open'] = df.groupby('symbol')["open"].resample("5T").first()
+    five_min_summary['high'] = df.groupby('symbol')["high"].resample("5T").max()
+    five_min_summary['low'] = df.groupby('symbol')["low"].resample("5T").min()
+    five_min_summary['close'] = df.groupby('symbol')["close"].resample("5T").last()
+
+    five_min_summary["interval"] = df.ix[4, 'interval'] = '5m'
+
+    # five_min_summary.head()
+    five_min_summary.to_sql('five_min_summary', engine, if_exists='replace')
 
 
-#10000 record test table
-df = pd.read_sql_query("select * from merged limit 10000;", engine)
-# df = pd.read_sql_query("select * from merged;", engine)
+    #15 MIN
+    fifteen_min_summary = pd.DataFrame()
+
+    fifteen_min_summary['open'] = df.groupby('symbol')["open"].resample("15T").first()
+    fifteen_min_summary['high'] = df.groupby('symbol')["high"].resample("15T").max()
+    fifteen_min_summary['low'] = df.groupby('symbol')["low"].resample("15T").min()
+    fifteen_min_summary['close'] = df.groupby('symbol')["close"].resample("15T").last()
+
+    fifteen_min_summary["interval"] = df.ix[4, 'interval'] = '15m'
+
+    # fifteen_min_summary.head()
+    fifteen_min_summary.to_sql('fifteen_min_summary', engine, if_exists='replace')
 
 
-# In[5]:
+    #1HR
+    hourly_summary = pd.DataFrame()
+
+    hourly_summary['open'] = df.groupby('symbol')["open"].resample("H").first()
+    hourly_summary['high'] = df.groupby('symbol')["high"].resample("H").max()
+    hourly_summary['low'] = df.groupby('symbol')["low"].resample("H").min()
+    hourly_summary['close'] = df.groupby('symbol')["close"].resample("H").last()
+
+    hourly_summary["interval"] = df.ix[4, 'interval'] = '1H'
+
+    # hourly_summary.head()
+    hourly_summary.to_sql('hourly_summary', engine, if_exists='replace')
 
 
-df = df.set_index(pd.DatetimeIndex(df['timestamp']))
+    #4HR
+    four_hour_summary = pd.DataFrame()
+
+    four_hour_summary['open'] = df.groupby('symbol')["open"].resample("4H").first().ffill()
+    four_hour_summary['high'] = df.groupby('symbol')["high"].resample("4H").max().ffill()
+    four_hour_summary['low'] = df.groupby('symbol')["low"].resample("4H").min().ffill()
+    four_hour_summary['close'] = df.groupby('symbol')["close"].resample("4H").last().ffill()
+
+    four_hour_summary["interval"] = df.ix[4, 'interval'] = '4H'
+
+    # four_hour_summary.head()
+    four_hour_summary.to_sql('four_hour_summary', engine, if_exists='replace')
 
 
-# In[6]:
+    #1 Day
+    daily_summary = pd.DataFrame()
+
+    daily_summary['open'] = df.groupby('symbol')["open"].resample("D").first().ffill()
+    daily_summary['high'] = df.groupby('symbol')["high"].resample("D").max().ffill()
+    daily_summary['low'] = df.groupby('symbol')["low"].resample("D").min().ffill()
+    daily_summary['close'] = df.groupby('symbol')["close"].resample("D").last().ffill()
+
+    daily_summary["interval"] = df.ix[4, 'interval'] = '1D'
+
+    # daily_summary.head()
+    daily_summary.to_sql('daily_summary', engine, if_exists='replace')
 
 
-df.head()
+    #1 Week
+    weekly_summary = pd.DataFrame()
+
+    weekly_summary['open'] = df.groupby('symbol')["open"].resample("W").first().ffill()
+    weekly_summary['high'] = df.groupby('symbol')["high"].resample("W").max().ffill()
+    weekly_summary['low'] = df.groupby('symbol')["low"].resample("W").min().ffill()
+    weekly_summary['close'] = df.groupby('symbol')["close"].resample("W").last().ffill()
+
+    weekly_summary["interval"] = df.ix[4, 'interval'] = '1W'
+
+    # weekly_summary.head()
+    weekly_summary.to_sql('weekly_summary', engine, if_exists='replace')
 
 
-# In[12]:
+    #1 Month
+    monthly_summary = pd.DataFrame()
+
+    monthly_summary['open'] = df.groupby('symbol')["open"].resample("M").first().ffill()
+    monthly_summary['high'] = df.groupby('symbol')["high"].resample("M").max().ffill()
+    monthly_summary['low'] = df.groupby('symbol')["low"].resample("M").min().ffill()
+    monthly_summary['close'] = df.groupby('symbol')["close"].resample("M").last().ffill()
+
+    monthly_summary["interval"] = df.ix[4, 'interval'] = '1M'
+
+    # monthly_summary.head()
+    monthly_summary.to_sql('monthly_summary', engine, if_exists='replace')
 
 
-#15 MIN
-fifteen_min_summary = pd.DataFrame()
+    #1 Year
+    yearly_summary = pd.DataFrame()
 
-fifteen_min_summary['open'] = df.open.resample('15T').first().ffill()
-fifteen_min_summary['high'] = df.high.resample('15T').max().ffill()
-fifteen_min_summary['low'] = df.low.resample('15T').min().ffill()
-fifteen_min_summary['close'] = df.close.resample('15T').last().ffill()
+    yearly_summary['open'] = df.groupby('symbol')["open"].resample("AS").first().ffill()
+    yearly_summary['high'] = df.groupby('symbol')["high"].resample("AS").max().ffill()
+    yearly_summary['low'] = df.groupby('symbol')["low"].resample("AS").min().ffill()
+    yearly_summary['close'] = df.groupby('symbol')["close"].resample("AS").last().ffill()
 
-fifteen_min_summary["interval"] = df.ix[4, 'interval'] = '15m'
+    yearly_summary["interval"] = df.ix[4, 'interval'] = '1Y'
 
-# fifteen_min_summary.head()
-fifteen_min_summary.to_sql('fifteen_min_summary', engine, if_exists='replace')
-
-
-# In[14]:
+    # yearly_summary.head()
+    yearly_summary.to_sql('yearly_summary', engine, if_exists='replace')
 
 
-#1 HR
-hourly_summary = pd.DataFrame()
 
-hourly_summary['open'] = df.open.resample('H').first().ffill()
-hourly_summary['high'] = df.high.resample('H').max().ffill()
-hourly_summary['low'] = df.low.resample('H').min().ffill()
-hourly_summary['close'] = df.close.resample('H').last().ffill()
+if __name__ == '__main__':
+    main()
 
-hourly_summary["interval"] = df.ix[4, 'interval'] = '1H'
-
-# hourly_summary.head()
-hourly_summary.to_sql('hourly_summary', engine, if_exists='replace')
-
-
-# In[15]:
-
-
-#4 HR
-four_hr_summary = pd.DataFrame()
-
-four_hr_summary['open'] = df.open.resample('4H').first().ffill()
-four_hr_summary['high'] = df.high.resample('4H').max().ffill()
-four_hr_summary['low'] = df.low.resample('4H').min().ffill()
-four_hr_summary['close'] = df.close.resample('4H').last().ffill()
-
-four_hr_summary["interval"] = df.ix[4, 'interval'] = '4H'
-
-# four_hr_summary.head()
-four_hr_summary.to_sql('four_hr_summary', engine, if_exists='replace')
-
-
-# In[16]:
-
-
-#1 DAY
-day_summary = pd.DataFrame()
-
-day_summary['open'] = df.open.resample('D').first().ffill()
-day_summary['high'] = df.high.resample('D').max().ffill()
-day_summary['low'] = df.low.resample('D').min().ffill()
-day_summary['close'] = df.close.resample('D').last().ffill()
-
-day_summary["interval"] = df.ix[4, 'interval'] = '1D'
-
-# day_summary.head()
-day_summary.to_sql('day_summary', engine, if_exists='replace')
-
-
-# In[17]:
-
-
-#1 week
-weekly_summary = pd.DataFrame()
-
-weekly_summary['open'] = df.open.resample('W').first().ffill()
-weekly_summary['high'] = df.high.resample('W').max().ffill()
-weekly_summary['low'] = df.low.resample('W').min().ffill()
-weekly_summary['close'] = df.close.resample('W').last().ffill()
-
-weekly_summary["interval"] = df.ix[4, 'interval'] = '1W'
-
-# weekly_summary.head()
-weekly_summary.to_sql('weekly_summary', engine, if_exists='replace')
-
-
-# In[18]:
-
-
-#1 MO
-month_summary = pd.DataFrame()
-
-month_summary['open'] = df.open.resample('M').first().ffill()
-month_summary['high'] = df.high.resample('M').max().ffill()
-month_summary['low'] = df.low.resample('M').min().ffill()
-month_summary['close'] = df.close.resample('M').last().ffill()
-
-month_summary["interval"] = df.ix[4, 'interval'] = '1M'
-
-# month_summary.head()
-month_summary.to_sql('month_summary', engine, if_exists='replace')
-
-
-# In[19]:
-
-
-#1 YR
-
-yearly_summary = pd.DataFrame()
-
-yearly_summary['open'] = df.open.resample('AS').first().ffill()
-yearly_summary['high'] = df.high.resample('AS').max().ffill()
-yearly_summary['low'] = df.low.resample('AS').min().ffill()
-yearly_summary['close'] = df.close.resample('AS').last().ffill()
-
-yearly_summary["interval"] = df.ix[4, 'interval'] = '1Y'
-
-# yearly_summary.head()
-yearly_summary.to_sql('yearly_summary', engine, if_exists='replace')
 
---
+++ sql/prepareTbl.sql
@@ -0,0 +1,18 @@
+﻿DROP TABLE test;
+DROP TABLE test2;
+SELECT * into test2 FROM merged LIMIT 1000000; 
+
+SELECT 
+symbol,
+open,
+high,
+low,
+closed,
+(d || ' ' || t) AS timestamp
+into test
+FROM test2;
+
+
+ALTER TABLE test ADD COLUMN interval character varying(10)
+ALTER TABLE test RENAME COLUMN closed TO close;
+SELECT * FROM "5m_test";
